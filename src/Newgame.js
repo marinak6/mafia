4c6295c26d6a423b0cc3152999b23b37bb7c8bb8
Noah French <noahjfrench@gmail.com>
2018-10-14 17:58:54 -0400

    Update packages and pick create a new game font.

--- src/Newgame.js
+++ src/Newgame.js
@@ -4,6 +4,6 @@ import { Button } from "antd";
 
 export default class Newgame extends Component {
   render() {
-    return <div>New Game Page!</div>;
+    return <div className="page-header">Create a New Game</div>;
   }
 }
