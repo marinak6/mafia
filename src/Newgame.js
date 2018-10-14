e7a3fa1eb672d1c8abf8ab70d9a667eb7a8f066f
Noah French <noahjfrench@gmail.com>
2018-10-14 18:49:25 -0400

    Make the basic formatting of the Newgame page.

--- src/Newgame.js
+++ src/Newgame.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
 import "./Main.css";
-import { Button } from "antd";
+import { Button, Form, Input, Icon, RadioGroup, Radio } from "antd";
 
 export default class Newgame extends Component {
   render() {
-    return <div className="page-header">Create a New Game</div>;
+    return (
+      <div>
+        <div className="page-header">Create a New Game</div>
+        <div className="newgame-form">
+          <Form>
+            <Form.Item label="Name:">
+              <Input
+                prefix={
+                  <Icon type="smile" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder="Name"
+              />
+            </Form.Item>
+            <Form.Item label="Setting:">
+              <Radio.Group>
+                <Radio value="camp-ground">Camp Ground</Radio>
+                <Radio value="submarine">Submarine</Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" className="create-game-button">
+                Create Game
+              </Button>
+              <Button>Back</Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    );
   }
 }
