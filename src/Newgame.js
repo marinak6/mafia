efced64de74cf8e2b1c41eb886946f835e642e2d
Noah French <noahjfrench@gmail.com>
2018-10-21 17:35:40 -0400

    Tweak Newgame page styling

--- src/Newgame.js
+++ src/Newgame.js
@@ -4,26 +4,34 @@ import { Button, Form, Input, Icon, RadioGroup, Radio } from "antd";
 
 export default class Newgame extends Component {
   render() {
+    const radioStyle = {
+      display: "block",
+      height: "30px",
+      lineHeight: "30px"
+    };
     return (
       <div>
         <div className="page-header">Create a New Game</div>
         <div className="newgame-form">
           <Form>
-            <Form.Item label="Name:">
-              <Input
-                prefix={
-                  <Icon type="smile" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                placeholder="Name"
-              />
+            <Form.Item>
+              <Input placeholder="Your Name" />
             </Form.Item>
-            <Form.Item label="Setting:">
+            <div className="sub-header">Setting:</div>
+            <Form.Item>
               <Radio.Group>
-                <Radio value="camp-ground">Camp Ground</Radio>
-                <Radio value="submarine">Submarine</Radio>
+                <Radio style={radioStyle} value="camp-ground">
+                  Camp Ground
+                </Radio>
+                <Radio style={radioStyle} value="submarine">
+                  Submarine
+                </Radio>
+                <Radio style={radioStyle} value="arctic">
+                  Arctic Research Laboratory
+                </Radio>
               </Radio.Group>
             </Form.Item>
-            <Form.Item>
+            <Form.Item className="newgame-buttons">
               <Button type="primary" className="create-game-button">
                 Create Game
               </Button>
